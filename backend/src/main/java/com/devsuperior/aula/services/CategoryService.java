9ee5de7415cf2aa2473ea7a08c02cd5422a84a24
Luann2003 <lvictorluciano@outlook.com>
2023-09-06 11:30:06 -0300

    findById

--- backend/src/main/java/com/devsuperior/aula/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/aula/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.devsuperior.aula.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.aula.entities.Category;
+import com.devsuperior.aula.entities.Dto.CategoryDTO;
 import com.devsuperior.aula.repositories.CategoryRepository;
 
 @Service
@@ -14,7 +17,16 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll(){
+		List<Category> result = repository.findAll();
+		return result.stream().map(x -> new CategoryDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> result = repository.findById(id);
+		Category entity = result.get();
+		return new CategoryDTO(entity);
 	}
 }
