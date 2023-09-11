32fe955c95c9f70ea61424b121776f0680a5e5b0
Luann2003 <lvictorluciano@outlook.com>
2023-09-11 09:01:51 -0300

    update / put

--- backend/src/main/java/com/devsuperior/aula/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/aula/services/CategoryService.java
@@ -30,4 +30,20 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
+	
+	@Transactional
+	public CategoryDTO insert (CategoryDTO dto) {
+		Category category = new Category();
+		category.setName(dto.getName());
+		category = repository.save(category);
+		return new CategoryDTO(category);
+	}
+	
+	@Transactional
+	public CategoryDTO update (Long id, CategoryDTO dto) {
+		Category category = repository.getReferenceById(id);
+		category.setName(dto.getName());
+		category = repository.save(category);
+		return new CategoryDTO(category);
+	}
 }
